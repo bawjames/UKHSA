3d22c8fb181d6a53a11a766325b0508779e16edc
Luke Boothby <lukeboothby23@gmail.com>
2026-03-13 10:59:19 +0000

    Added Register Page

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -11,7 +11,7 @@ public class LoginController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Register()
     {
         return View();
     }
