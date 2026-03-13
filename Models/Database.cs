7d3d869812543ff9f3e14581354eaceb62f8d6ed
James Baw <james+git@jamesbaw.com>
2026-03-13 12:10:14 +0000

    Fixed bad rebase

--- Models/Database.cs
+++ Models/Database.cs
@@ -1,10 +1,18 @@
 namespace UKHSA.Models;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-public class User
+public class User : IdentityUser
 {
-    public int Id { get; set; }
-    public Role Role { get; set; }
+    // NOTE: Id field is inherited from IdentityUser
+
+    public required string Forename { get; set; }
+    public required string Surname { get; set; }
+    public required List<Role> Roles { get; set; }
+
+    [InverseProperty("User")]
+    public required List<Request> Requests { get; set; }
 }
 
 public enum Role
@@ -17,13 +25,33 @@ public enum Role
 public class Request
 {
     public int Id { get; set; }
+    public DateTime RequestDateTime { get; set; }
+
+    [ForeignKey("Approval")]
+    public int ApprovalId { get; set; }
+    [ForeignKey("User")]
+    public required int UserId { get; set; }
+    [ForeignKey("Dataset")]
+    public required int DatasetId { get; set; }
 }
 
 public class Dataset
 {
     public int Id { get; set; }
-}
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public int AccessLevel { get; set; }
+
+    [InverseProperty("Dataset")]
+    public required List<Request> Requests { get; set; }
+ }
 
 public class Approval
 {
+    public bool Approved { get; set; }
+    public DateTime ApprovedDateTime { get; set; }
+    public string? RejectedReason { get; set; }
+
+    [InverseProperty("Approval")]
+    public required Request Requests { get; set; }
 }
