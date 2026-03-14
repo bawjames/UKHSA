5b3dc3d05ce75e1ca596fffd107cc781acad366f
James Baw <james+git@jamesbaw.com>
2026-03-14 12:13:19 +0000

    Added database migrations to Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -4,10 +4,12 @@ using UKHSA.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Connect to Postgres
 builder.Services.AddDbContext<UKHSA_DbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+// Configure Login
 builder.Services.AddIdentity<UKHSA.Models.User, IdentityRole>()
     .AddEntityFrameworkStores<UKHSA_DbContext>()
     .AddDefaultTokenProviders();
@@ -15,18 +17,26 @@ builder.Services.AddIdentity<UKHSA.Models.User, IdentityRole>()
 builder.Services.AddAuthentication()
     .AddCookie(options => options.LoginPath = "/Account/Login");
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Perform EFCore database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<UKHSA_DbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
+// Redirect from root
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}")
