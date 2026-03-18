2fc8786a3a31c0882f7156a0154c84fe02914d4e
James Baw <james+git@jamesbaw.com>
2026-03-18 16:30:52 +0000

    Added pagination to the Requests page

--- App/Controllers/UserController.cs
+++ App/Controllers/UserController.cs
@@ -5,6 +5,16 @@ using UKHSA.Models;
 
 namespace UKHSA.Controllers;
 
+// Move this to a different namespace
+public class Paginated<T>
+{
+    public List<T> Items { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+}
+
 public class UserController : Controller
 {
     protected readonly UKHSA_DbContext _context;
@@ -18,18 +28,28 @@ public class UserController : Controller
 
     public IActionResult Home()
     {
-        var users = _context.Users.ToList();
-
-        return View(users);
+        return View();
     }
 
-    public IActionResult Requests()
+    public IActionResult Requests(int page = 1, int pageSize = 20)
     {
         var requests = _context.Requests
-        .Where(r => r.UserId == _userManager.GetUserId(User))
-        .ToList();
+                       .Where(r => r.UserId == _userManager.GetUserId(User))
+                       .Skip((page - 1) * pageSize)
+                       .Take(pageSize)
+                       .ToList();
 
-        return View(requests);
+        int totalItems = _context.Requests.Count();
+        int totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+        var model = new Paginated<Request> {
+            CurrentPage = page,
+            PageSize = pageSize,
+            TotalItems = totalItems,
+            Items = requests,
+        };
+
+        return View(model);
     }
 
     public IActionResult RequestDocument()
