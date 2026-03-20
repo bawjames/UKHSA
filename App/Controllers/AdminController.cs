b50f23d59b713a3a7d16d7f036360e1fc8a0eed3
Luke Boothby <lukeboothby23@gmail.com>
2026-03-20 11:31:53 +0000

    Fixed admin controller

--- App/Controllers/AdminController.cs
+++ App/Controllers/AdminController.cs
@@ -9,23 +9,6 @@ public class AdminController : Controller
     protected readonly UKHSA_DbContext _context;
     public AdminController(UKHSA_DbContext context) => _context = context;
 
-    public IActionResult Users(int page = 1, int perPage = 20)
-    {
-        int totalItems = _context.Users.Count();
-
-        var allUsers = _context.Users
-                          .ToList();
-
-        var model = new Paginated<Users> {
-            CurrentPage = page,
-            PerPage = perPage,
-            TotalItems = totalItems,
-            Items = allRequests,
-        };
-
-        return View(model);
-    }
-
     public IActionResult AddDocument()
     {
         var documents = _context.Datasets.ToList;
