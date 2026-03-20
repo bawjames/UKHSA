98cf8d1afb6bdc1e04a8566a54ebcacccac36968
Luke Boothby <lukeboothby23@gmail.com>
2026-03-20 11:20:38 +0000

    Added Pagination and db intergation to Approve Requests

--- App/Controllers/ApproverController.cs
+++ App/Controllers/ApproverController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using UKHSA.Models;
+using UKHSA.Shared;
 
 namespace UKHSA.Controllers;
 
@@ -9,10 +10,20 @@ public class ApproverController : Controller
      protected readonly UKHSA_DbContext _context;
     public ApproverController(UKHSA_DbContext context) => _context = context;
 
-    public IActionResult ApproveRequest()
+    public IActionResult ApproveRequest(int page = 1, int perPage = 20)
     {
+        
+        int totalItems = _context.Requests.Count();
         var requests = _context.Requests.ToList();
-        return View(requests);
+
+        var model = new Paginated<Request> {
+            CurrentPage = page,
+            PerPage = perPage,
+            TotalItems = totalItems,
+            Items = requests,
+        };
+
+        return View(model);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
